79d63805f345bbd452568ab8e060ee8e1c4bde13
hmdns26 <hmd996hh@gmail.com>
2023-12-27 19:03:13 +0330

    add current number

--- script.js
+++ script.js
@@ -14,12 +14,23 @@ const btnHold = document.querySelector('.btn--hold');
 const score = [0,0] ;
 let currentScore = 0 ;
 let activePlayer = 0 ;
+let playing = true ;
 
 score0El.textContent = 0 ;
 score1El.textContent = 0 ;
 diceEl.classList.add('hidden');
 
+const switchPlayer =function(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0 ;
+    currentScore =0 ;
+    activePlayer = activePlayer === 0 ? 1 : 0 ;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+
+
 btnRoll.addEventListener('click', function(){
+    if(playing){
     const dice = Math.trunc(Math.random()*6)+1 ;
     
     diceEl.classList.remove('hidden');
@@ -29,10 +40,20 @@ btnRoll.addEventListener('click', function(){
         currentScore+=dice ;
         document.getElementById(`current--${activePlayer}`).textContent = currentScore ;
     }else{
-        document.getElementById(`current--${activePlayer}`).textContent = 0 ;
-        currentScore =0 ;
-        activePlayer = activePlayer === 0 ? 1 : 0 ;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        switchPlayer();
     }
+}
 });
+btnHold.addEventListener('click',function(){
+    if(playing){
+    score[activePlayer] += currentScore ;
+    document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer];
+    if(score[activePlayer] >=20){
+        playing=false;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+    }else{
+        switchPlayer();
+    }
+}
+})
